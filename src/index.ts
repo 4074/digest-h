0332971c89e3d654f71ad29add2e19aac8ee027d
4074 <win.ten.in@gmail.com>
2025-03-08 02:43:07 +0800

    feat: add validDigest

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,10 @@ import crypto from 'crypto'
 
 export const md5 = (s: string) => crypto.createHash('md5').update(s).digest('hex')
 
-export interface CreateDigestOptions {}
-
-export interface CreateDigestSource {
+export interface DigestParts {
   realm: string
   username: string
   password: string
-  method: 'GET' | 'POST'
   uri: string
   nonce?: string
   cnonce?: string
@@ -17,7 +14,11 @@ export interface CreateDigestSource {
   opaque?: string
 }
 
-const createCNonce = () => Date.now().toString()
+export interface CreateDigestSource extends DigestParts {
+  method: string
+}
+
+const createCnonce = () => Date.now().toString()
 
 export const createDigest = ({
   realm,
@@ -26,13 +27,13 @@ export const createDigest = ({
   method,
   uri,
   nonce,
-  cnonce = createCNonce(),
+  cnonce = createCnonce(),
   nc,
   qop = 'auth',
   opaque
 }: CreateDigestSource) => {
   const ha1 = md5([username, password].join(':'))
-  const ha2 = md5([method, uri].join(':'))
+  const ha2 = md5([method.toUpperCase(), uri].join(':'))
   const response = md5([ha1, nonce, nc, cnonce, qop, ha2].filter(Boolean).join(':'))
   const keys = ['username', 'realm', 'nonce', 'uri', 'qop', 'nc', 'cnonce', 'response', 'opaque']
   const values = {
@@ -52,7 +53,50 @@ export const createDigest = ({
     .map((k) => {
       const v = typeof values[k] === 'number' ? values[k].toString() : `"${values[k]}"`
       return `${k}=${v}`
-    })}`
+    })
+    .join(', ')}`
 
   return digest
+}
+
+const PART_RE = `(\\w+)=["']?([^'"]{1,10000})["']?`
+const getParts = (s: string) => {
+  const pairs = s.match(new RegExp(PART_RE, 'g'))
+  const parts = {}
+  for (const pair of pairs) {
+    const [, k, v] = pair.match(new RegExp(PART_RE))
+    parts[k] = v
+  }
+  return parts as DigestParts & { response: string }
+}
+
+export interface ValidDigestSource {
+  method: string
+  digest: string
+  password: string
+}
+
+export interface ValidDigestOptions {
+  isCnonceDate?: boolean
+  expried?: number
+  needs?: (keyof DigestParts)[]
+}
+
+export const validDigest = ({ method, digest, password }: ValidDigestSource, options: ValidDigestOptions = {}) => {
+  const { expried = 60, isCnonceDate = true, needs = ['username', 'realm', 'uri', 'qop', 'cnonce'] } = options
+  const parts = getParts(digest)
+  if (!needs.reduce((r, k) => r && Boolean(parts[k]), true)) {
+    return false
+  }
+  if (isCnonceDate && Date.now() - Number(parts.cnonce) > expried * 1000) {
+    return false
+  }
+  const localParts = getParts(
+    createDigest({
+      ...parts,
+      method,
+      password
+    })
+  )
+  return parts.response === localParts.response
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { createDigest } from '../src'
+import { createDigest, validDigest } from '../src'
 
 describe('createDigest', () => {
   it('should pass by mini options', () => {
@@ -12,7 +12,7 @@ describe('createDigest', () => {
         cnonce: '1741284305531'
       })
     ).toEqual(
-      'Digest username="user",realm="digest-h",uri="/index",qop="auth",cnonce="1741284305531",response="76c051465a85590a7dd2355cf77588e7"'
+      'Digest username="user", realm="digest-h", uri="/index", qop="auth", cnonce="1741284305531", response="76c051465a85590a7dd2355cf77588e7"'
     )
   })
 
@@ -26,7 +26,89 @@ describe('createDigest', () => {
         uri: '/index'
       })
     ).not.toEqual(
-      'Digest username="user",realm="digest-h",uri="/index",qop="auth",cnonce="1741284305531",response="76c051465a85590a7dd2355cf77588e7"'
+      'Digest username="user",realm="digest-h", uri="/index", qop="auth", cnonce="1741284305531", response="76c051465a85590a7dd2355cf77588e7"'
     )
+  })
+})
+
+describe('createDigest', () => {
+  it('should not valid cnone expried', () => {
+    expect(
+      validDigest({
+        method: 'GET',
+        digest:
+          'Digest username="user",realm="digest-h",uri="/index",qop="auth",cnonce="1741284305531",response="76c051465a85590a7dd2355cf77588e7"',
+        password: '123456'
+      })
+    ).toBe(false)
+  })
+
+  it('should not valid with wrong password', () => {
+    expect(
+      validDigest({
+        method: 'GET',
+        digest: createDigest({
+          realm: 'digest-h',
+          username: 'user',
+          password: '123456',
+          method: 'GET',
+          uri: '/index'
+        }),
+        password: '654321'
+      })
+    ).toBe(false)
+  })
+
+  it('should valid with normal case', () => {
+    expect(
+      validDigest({
+        method: 'GET',
+        digest: createDigest({
+          realm: 'digest-h',
+          username: 'user',
+          password: '123456',
+          method: 'GET',
+          uri: '/index'
+        }),
+        password: '123456'
+      })
+    ).toBe(true)
+  })
+
+  it('should support lower case method', () => {
+    expect(
+      validDigest({
+        method: 'get',
+        digest: createDigest({
+          realm: 'digest-h',
+          username: 'user',
+          password: '123456',
+          method: 'GET',
+          uri: '/index'
+        }),
+        password: '123456'
+      })
+    ).toBe(true)
+  })
+
+  it('should support needs', () => {
+    expect(
+      validDigest(
+        {
+          method: 'get',
+          digest: createDigest({
+            realm: 'digest-h',
+            username: 'user',
+            password: '123456',
+            method: 'GET',
+            uri: '/index'
+          }),
+          password: '123456'
+        },
+        {
+          needs: ['username', 'realm', 'uri', 'qop', 'cnonce', 'nonce']
+        }
+      )
+    ).toBe(false)
   })
 })
